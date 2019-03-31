4064c25fe49d781766d6a3d7abbaff255275d735
ryan4yin <xiaoyin_c@qq.com>
2019-03-31 19:36:36 +0800

    finish conveter: cq_2_xhup

--- converter.py
+++ converter.py
@@ -4,27 +4,33 @@
 进行 xhup-club-api 与 cqhttp 之间的消息转换
 """
 
-update_format = {
-    "platform": 'qq',  # or 'telegram'、'wechat'
-    "type": "message",  # or 'notice'
-    "message": {  # 如果 update 类型是 message
-        "type": "private",  # or 'group'
-        "user": {
-            "id": "",  # 用户 id，QQ 号等
-            "role": "owner",  # 群组 owner/admin/other
-        },
-        "group": {
-            "id": "",  # 群 id
-            "at_me": True,  # 是否是 at 我
-        },
+from config import BotConfig
 
-        "text": "",  # 消息的 text 部分。（去除掉了表情、at 和多媒体数据）
-        "photo": "",  # 图片路径
-    },
-    "notice": {  # 如果 update 类型是 notice 的话
-        # TODO 暂时未实现
-    }
-}
+
+def is_at_me(msg, self_id):
+    self_id = int(self_id)
+    for item in msg:
+        if item['type'] == 'at' \
+                and int(item['data']['qq']) == self_id:
+            return True
+        elif item['type'] == 'text' \
+                and BotConfig.NICKNAME in item['data']['text']:
+            return True
+
+    return False
+
+
+def extract_images(msg):
+    res = []
+    for item in msg:
+        if item['type'] == 'image':
+            img = item['data']
+            res.append({
+                "filename": img['file'],
+                "url": img['url'],
+            })
+
+    return res
 
 
 def cq_2_xhup(info):
@@ -33,8 +39,37 @@ def cq_2_xhup(info):
     :param info:
     :return:
     """
+    res = {
+        "platform": 'qq',
+        "type": info['post_type'],
+    }
 
-    return info
+    if res['type'] == "message":
+        return {
+            **res,
+            "message": {  # 如果 update 类型是 message
+                "type": info.get("message_type"),  # 'private' or 'group'
+                "user": {
+                    "id": info.get('user_id'),  # 用户 id，QQ 号等
+                    "role": info.get('sender'),  # 群组 owner/admin/other
+                },
+                "group": {
+                    "id": info['group_id'],  # 群 id
+                    "at_me": is_at_me(info['message'], info['self_id']),  # 是否是 at 我
+                },
+
+                "text": info['message'].extract_plain_text(),  # 消息的 text 部分。（去除掉了表情、at 和多媒体数据）
+                "images": extract_images(info['message']),  # 图片路径
+            },
+        }
+    elif res['type'] == "notice":
+        pass
+        # return {
+        #     **res,
+        #     "notice": {  # 如果 update 类型是 notice 的话
+        #         # TODO 暂时未实现
+        #     }
+        # }
 
 
 def xhup_2_cq(info):
